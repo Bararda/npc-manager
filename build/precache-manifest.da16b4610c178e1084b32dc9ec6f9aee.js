2cd08caf44d4e4eeb18b75977241da5bb4a8552a
Bararda <anthonydoucet5417@hotmail.com>
2020-09-04 22:07:19 -0700

    created build

---
+++ build/precache-manifest.da16b4610c178e1084b32dc9ec6f9aee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3deae26cff6f22279977d5d3150793a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b2059e2b43b7d8dec7a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ba8105785d60ae7d7174",
+    "url": "/static/css/main.b0c1d8a1.chunk.css"
+  },
+  {
+    "revision": "8b2059e2b43b7d8dec7a",
+    "url": "/static/js/2.a3a8df80.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a3a8df80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8105785d60ae7d7174",
+    "url": "/static/js/main.cac7560a.chunk.js"
+  },
+  {
+    "revision": "7db3ac355079c9fda765",
+    "url": "/static/js/runtime-main.ca7f7c79.js"
+  },
+  {
+    "revision": "7c5967e34a7659add288472f4439827b",
+    "url": "/static/media/blue_wings.7c5967e3.png"
+  }
+]);
